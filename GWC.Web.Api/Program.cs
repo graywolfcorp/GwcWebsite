9999b21991f19f3bdf46adfef8367d0072aaccb4
Park Espenschade <park@graywolfcorp.com>
2018-12-30 10:22:08 -0500

    user secrets

--- GWC.Web.Api/Program.cs
+++ GWC.Web.Api/Program.cs
@@ -1,15 +1,13 @@
-﻿using GWC.Web.DataAccess;
+using GWC.Web.DataAccess;
+using GWC.Web.Dtos;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.IO;
 
 namespace GWC.Web.Api
 {
-#pragma warning disable 1591
     public class Program
     {
         public static void Main(string[] args)
@@ -21,30 +19,26 @@ namespace GWC.Web.Api
             WebHost.CreateDefaultBuilder(args)
                 .UseKestrel()
                 .UseAzureAppServices()
-                //.UseIISIntegration()
                 .UseStartup<Startup>()
                 .UseApplicationInsights()                
                 .Build();
 
-        //must change ASPNETCORE_ENVIRONMENT in package manage console to update production with update-database
-        //$Env:ASPNETCORE_ENVIRONMENT = "DevelopmentX"
         public class GwcDbContexttFactory : IDesignTimeDbContextFactory<GwcDbContext>
         {
             public GwcDbContext CreateDbContext(string[] args)
-            {                
-                var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-                var jsonFile = environmentName == "Development" ? "appsettings.Development.json" : "appsettings.json";
-
-                IConfigurationRoot configuration = new ConfigurationBuilder()
-                    .SetBasePath(Directory.GetCurrentDirectory())
-                    .AddJsonFile(jsonFile, optional:false, reloadOnChange: true)
+            {
+                var webConfiguration = new WeblogConfigurationDto();
+                var configuration = new ConfigurationBuilder()
+                    .AddUserSecrets<Startup>()
+                    .AddEnvironmentVariables()
                     .Build();
-                var builder = new DbContextOptionsBuilder<GwcDbContext>();
-                var connectionString = configuration.GetConnectionString("graywolf");
-                builder.UseSqlServer(connectionString);
+
+                configuration.GetSection("AppSettings").Bind(webConfiguration);
+
+                var builder = new DbContextOptionsBuilder<GwcDbContext>();                
+                builder.UseSqlServer(webConfiguration.ConnectionString);
                 return new GwcDbContext(builder.Options);
             }
         }
     }
-#pragma warning restore 1591
 }
